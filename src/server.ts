78779951a186b22d70252610363937c6c42bf669
Niel Rohling <niel.rohling@gmail.com>
2020-12-21 16:24:24 +0100

    create db file if it doesnt exisits

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,25 @@
-import { ExpressApp } from "./api/ExpressApp";
+import path from 'path';
+import fs from 'fs';
 
-const api = new ExpressApp();
-api.start(9000);
+import { ExpressApp } from './api/ExpressApp';
+
+const json = JSON.stringify({
+  nextArticleID: 0,
+  nextCustomerID: 0,
+  nextTransactionID: 3,
+  customers: [],
+  articles: []
+});
+
+
+fs.writeFile(path.join(process.cwd(), 'kiosk.json'), json, { flag: 'wx' }, (err => {
+  if (err) {
+    if (err.code === 'EEXIST') {
+      console.log('not creating db-file, alredy exists');
+    } else {
+      console.log(err);
+    }
+  }
+  const api = new ExpressApp();
+  api.start(9000);
+}));
