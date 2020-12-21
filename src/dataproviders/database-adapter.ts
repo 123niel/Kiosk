9508cbc3accfc764c1ad5f39f2ed46a7cd503663
Niel Rohling <niel.rohling@gmail.com>
2020-12-21 16:25:10 +0100

    single quotes

--- src/dataproviders/database-adapter.ts
+++ src/dataproviders/database-adapter.ts
@@ -1,5 +1,6 @@
 import { JsonDB } from 'node-json-db';
 import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
+
 import { Article } from '../models/article';
 import { Customer } from '../models/customer';
 import { CartItem, Transaction } from '../models/transaction';
@@ -7,20 +8,20 @@ import { CartItem, Transaction } from '../models/transaction';
 export class DatabaseAdapter {
   private db: JsonDB;
   constructor() {
-    this.db = new JsonDB(new Config("kiosk.json", true, false));
+    this.db = new JsonDB(new Config('kiosk.json', true, false));
   }
 
   // Articles
   getArticles(): Article[] {
-    return this.db.getData("/articles");
+    return this.db.getData('/articles');
   }
 
   addArticle(article: Article): Article {
-    const id = this.db.getData("/nextArticleID");
+    const id = this.db.getData('/nextArticleID');
     article.id = id;
     article.disabled = false;
-    this.db.push("/articles[]", article, true);
-    this.db.push("/nextArticleID", id + 1);
+    this.db.push('/articles[]', article, true);
+    this.db.push('/nextArticleID', id + 1);
     return article;
   }
 
@@ -31,7 +32,7 @@ export class DatabaseAdapter {
   disableArticle(id: number, disabled: boolean): Article {
     const article = this.getArticleByID(id);
     article.disabled = disabled;
-    const index = this.db.getIndex("/articles", `${article.id}`);
+    const index = this.db.getIndex('/articles', `${article.id}`);
     this.db.push(`/articles[${index}]`, article, true);
     return article;
   }
@@ -39,15 +40,15 @@ export class DatabaseAdapter {
   // Customers
 
   getCustomers(): Customer[] {
-    return this.db.getData("/customers");
+    return this.db.getData('/customers');
   }
 
   addCustomer(customer: Customer): Customer {
-    const id = this.db.getData("/nextCustomerID");
+    const id = this.db.getData('/nextCustomerID');
     customer.id = id;
     customer.transactions = [];
-    this.db.push("/customers[]", customer, true);
-    this.db.push("/nextCustomerID", id + 1);
+    this.db.push('/customers[]', customer, true);
+    this.db.push('/nextCustomerID', id + 1);
     return customer;
   }
 
@@ -61,14 +62,14 @@ export class DatabaseAdapter {
   }
 
   addTransaction(customerId: number, cart: CartItem[], deposit: number, time: Date): Transaction {
-    const id = this.db.getData("/nextTransactionID");
+    const id = this.db.getData('/nextTransactionID');
     const action: Transaction = { id, cart, deposit, time };
 
     const customers = this.getCustomers();
     const customer = customers.filter(c => c.id === customerId)[0];
     const index = customers.indexOf(customer);
     this.db.push(`/customers[${index}]/transactions[]`, action, true);
-    this.db.push("/nextTransactionID", id + 1);
+    this.db.push('/nextTransactionID', id + 1);
     return action;
   }
 }
